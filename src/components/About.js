e1652831e159ca9df9c04b82badf83e5b7fb4dcd
ayman fakri <61848289+afakri@users.noreply.github.com>
2022-01-25 16:10:18 -0500

    updated header

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,31 @@
 import React from "react";
-import ReactTypingEffect from 'react-typing-effect';
+import ReactTypingEffect from "react-typing-effect";
 
 function About() {
-  const desc = "I'm a third year software engineering student at the University of Ottawa,working on my startup idea while also pursuing my degree. I enjoy building projects while learning new technologies.I'm currently seeking an internship for Fall 2021."
-   
+  const desc =
+    "I'm a third year software engineering student at the University of Ottawa,working on my startup idea while also pursuing my degree. I enjoy building projects while learning new technologies.I'm currently seeking an internship for Summer 2022.";
+
   return (
     <div className="About-section">
       <div className="small-header">
         <p>Hi, my name is </p>
       </div>
       <div className="Name">
-      <ReactTypingEffect
-        text={["Ayman Fakri"]}
-        typingDelay="100"
-        eraseDelay="700"
-        eraseSpeed="200"
-        speed="200"
-        
-        
-      />
+        <ReactTypingEffect
+          text={["Ayman Fakri"]}
+          typingDelay="100"
+          eraseDelay="700"
+          eraseSpeed="200"
+          speed="200"
+        />
       </div>
       <div className="below-name-container">
-          <p className="Name below-name">Computer Science Enthusiast </p>  
-    
+        <p className="Name below-name">Computer Science Enthusiast </p>
       </div>
-      
+
       <div className="about-info-container">
         <p className="about-info">{desc}</p>
       </div>
-      
     </div>
   );
 }
